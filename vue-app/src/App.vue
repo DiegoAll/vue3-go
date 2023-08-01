54266c016f5fa43a3472f7a7e75ba22d1d6ba5ab
diegoapl <diego.posada@mercadolibre.com.co>
2023-07-31 19:53:48 -0500

    vue-cli application structure

---
+++ vue-app/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+
+
+<div class="container">
+  <div class="row">
+    <div class="col">
+      <h1>hola</h1>
+    </div>
+  </div>
+</div>
+<h2>hola pana</h2>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
