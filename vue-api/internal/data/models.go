ad19ba3f5ee2f1c02d34812f9c8f9b6a2f44f753
DiegoAll <dposadallano@gmail.com>
2023-08-06 01:08:16 -0500

    Adding a Token model

--- vue-api/internal/data/models.go
+++ vue-api/internal/data/models.go
@@ -9,5 +9,17 @@ type User struct {
 	Lastname  string    `json:"last_name,omitempty"`
 	Password  string    `json:"password"`
 	CreatedAt time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"update_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	Token     Token     `json:"token"`
+}
+
+type Token struct {
+	ID        int       `json:"id"`
+	UserID    int       `json:"user_id"`
+	Email     string    `json:"email"`
+	Token     string    `json:"token"`
+	TokenHash []byte    `json:"-"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	Expiry    time.Time `json:"expiry"`
 }
