6eb41dac8a580d950a79510941e6ced1404fe2e5
DiegoAll <dposadallano@gmail.com>
2023-08-12 21:04:53 -0500

    Trying out generating a token [/test-generate-token]

--- vue-api/cmd/api/routes.go
+++ vue-api/cmd/api/routes.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	"time"
 	"vue-api/internal/data"
 
 	"github.com/go-chi/chi/v5"
@@ -57,6 +58,27 @@ func (app *application) routes() http.Handler {
 		app.infoLog.Println("Got back id of", id)
 		newUser, _ := app.models.User.GetOne(id)
 		app.writeJSON(w, http.StatusOK, newUser)
+	})
+
+	mux.Get("/test-generate-token", func(w http.ResponseWriter, r *http.Request) {
+		token, err := app.models.User.Token.GenerateToken(2, 60*time.Minute)
+		if err != nil {
+			app.errorLog.Println(err)
+			return
+		}
+
+		token.Email = "admin@example.com"
+		token.CreatedAt = time.Now()
+		token.UpdatedAt = time.Now()
+
+		payload := jsonResponse{
+			Error:   false,
+			Message: "success",
+			Data:    token,
+		}
+
+		app.writeJSON(w, http.StatusOK, payload)
+
 	})
 
 	return mux
